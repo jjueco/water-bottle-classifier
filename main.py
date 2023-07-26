138832c1b4fc4da5cb6328120c54904e074ca34f
jjueco <72325841+jjueco@users.noreply.github.com>
2023-07-26 08:21:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ import RPi.GPIO as GPIO
 
 #initiliaze GPIO
 pinOUt = 22
+pinIn = 23
 GPIO.setup(pinOut, GPIO.OUT)
+GPIO.setup(pinIn, GPIO.IN)
 
 
 #Load bottle classifier
@@ -18,8 +20,8 @@ def output_converter(model_output):
 
     output = model_output
 
-    # assume that 'output' is a numpy array of shape (n, 3)
-    output_labels = ['0.5 L', '1 L', '1.5 L', '2 L']
+    # assume that 'output' is a numpy array of shape (n, 2)
+    output_labels = ['0.5 L', '1 L']
     predictions = np.argmax(output, axis=1)
     predicted_labels = [output_labels[p] for p in predictions]
 
@@ -38,6 +40,7 @@ while True:
   if ret:
 
       # Resize the image to 64x64
+      full_image = cv2.resize(frame, interpolation=cv2.INTER_CUBIC)
       rescaled_image = cv2.resize(frame, dsize=(64, 64), interpolation=cv2.INTER_CUBIC)
 
       # Convert the image to an array
@@ -47,8 +50,9 @@ while True:
       cv2.imshow("Image", rescaled_image)
 
       # Save the image
-      save_directory = "/my/new/directory"
+      save_directory = "/home/pi/github"
       cv2.imwrite(os.path.join(save_directory, "image.jpg"), rescaled_image)
+      cv2.imwrite(os.path.join(save_directory, "image_full.jpg"), full_image)
 
       # Wait for a key press
       cv2.waitKey(0)
@@ -56,16 +60,23 @@ while True:
   # Release the VideoCapture object
   cap.release()
   
-  model = tf.keras.models.load_model("/content/images/self_train.h5")
+  model = tf.keras.models.load_model("/home/pi/github/self_train.h5")
   prediction_label = output_converter(model.predict(image_array.reshape(1, 64, 64, 3)))
   
   if prediction_label = '0.5 L':
     label = 0
     GPIO.output(pinOut, GPIO.LOW)
-    print('Bottle is 0.5 L.')
+    if GPIO.input(pinIn, GPIO.LOW):
+      print('Bottle is 0.5 L.')
+    else:
+      print('Bottle is 1 L.')
+
   else:
     label = 1
     GPIO.output(pinOut, GPIO.HIGH)
-    print('Bottle is 1 L.')
+    if GPIO.input(pinIn, GPIO.LOW):
+      print('Bottle is 1 L.')
+    else:
+      print('Bottle is 0.5 L.')
 
   time.sleep(10)
